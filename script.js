1a3631374c191c26a7793a0dbca3693223dea78e
Juanmyrock <juanmyrock@gmail.com>
2024-05-19 05:18:23 -0300

    otra section de cards
    agregando las cards para otra seccion que seran o de vuelos o de lo que sea, + script del "likeButton" + css modificados

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ likeButtons.forEach((likeButton) => {
     likeButton.classList.toggle("card__btn--like");
   });
 });
+
+const likeButtons2 = document.querySelectorAll(".card2_btn");
+
+likeButtons2.forEach((likeButton) => {
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card2_btn--like");
+  });
+});
